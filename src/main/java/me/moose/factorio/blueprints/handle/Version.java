f8a57703031da4972b42b4f3c10f22d9ad7f3d3c
Moose1301 <40875577+Moose1301@users.noreply.github.com>
2021-06-25 22:16:55 -0400

    Version info

--- src/main/java/me/moose/factorio/blueprints/handle/Version.java
+++ src/main/java/me/moose/factorio/blueprints/handle/Version.java
@@ -4,6 +4,23 @@ import lombok.Getter;
 
 import java.nio.ByteBuffer;
 //https://wiki.factorio.com/Version_string_format
+/*
+Each version number is 2 bytes so we get the second byte of that number.
+
+Major: [1]
+Minor: [3]
+Patch: [5]
+Developer Version Number: [7]
+EG:
+
+Version Long: 281479273971713
+Would Be
+Major: 1
+Minor: 3
+Patch: 35
+Developer Version Number: 1
+
+ */
 @Getter
 public class Version {
     private long version;
